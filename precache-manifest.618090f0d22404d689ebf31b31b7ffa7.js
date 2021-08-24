63a224973141959091103ee0274511440778445d
Nick Balistreri <balistrerin@gmail.com>
2021-08-23 22:26:41 -0700

    Updates

---
+++ precache-manifest.618090f0d22404d689ebf31b31b7ffa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f01663a1821e6697a18c",
+    "url": "/KittyCatsNFT/static/css/main.9381cb55.chunk.css"
+  },
+  {
+    "revision": "f01663a1821e6697a18c",
+    "url": "/KittyCatsNFT/static/js/main.f01663a1.chunk.js"
+  },
+  {
+    "revision": "402a2e77087e0e4a4428",
+    "url": "/KittyCatsNFT/static/css/1.dd8e08aa.chunk.css"
+  },
+  {
+    "revision": "402a2e77087e0e4a4428",
+    "url": "/KittyCatsNFT/static/js/1.402a2e77.chunk.js"
+  },
+  {
+    "revision": "7784f95215643ff52fa5",
+    "url": "/KittyCatsNFT/static/js/runtime~main.7784f952.js"
+  },
+  {
+    "revision": "0771acf31b761778abf3eafe66273830",
+    "url": "/KittyCatsNFT/static/media/meow.0771acf3.mp3"
+  },
+  {
+    "revision": "71723fafd0b7345dfaad0c54d38a0444",
+    "url": "/KittyCatsNFT/index.html"
+  }
+];
